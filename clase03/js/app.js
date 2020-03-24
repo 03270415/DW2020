e60850b6c98f99133278ce07ac48597c8e690d6a
03270415 <03270415>
2020-03-24 19:06:24 -0400

    clase02

---
+++ clase03/js/app.js
@@ -0,0 +1,108 @@
+var g="-------------------------";
+console.log(g);
+var tb="Imprimiendo tabla de 5. ";
+console.log(tb);
+var g="-------------------------";
+console.log(g);
+
+for(var i=0; i<11; i++)
+{
+  var num1=5;
+  var resul=num1*i;
+  console.log(num1+"x"+i+"="+resul);
+}
+var e="=========================";
+console.log(e);
+
+var g="-------------------------";
+console.log(g);
+var tb="Imprimiendo tabla de 6. ";
+console.log(tb);
+var g="-------------------------";
+console.log(g);
+
+var num2=6;
+var num3=10;
+if(num2<num3)
+{
+  for(var i=0; i<11; i++)
+  {
+  var resul2=num2*i;
+  console.log(num2+"x"+i+"="+resul2);
+  }
+}
+  else{
+    console.log("No es posible procesar.");
+      }
+var e="=========================";
+console.log(e);
+
+//alert("Atención!");
+//console.log("Atención!")// se puede controlar como se llevan los valores.
+//var nro01=5;
+//console.log(nro01);
+//var nro02="hola ";
+//console.log(nro01);
+//nro01=5
+//var resultado;
+//resultado=nro01+5;
+//console.log(resultado);
+//resultado=nro01-5;
+//console.log(resultado);
+//resultado=nro01*5;
+//console.log(resultado);
+//resultado=nro01/5;
+//console.log(resultado);
+//resultado=nro01+"-"+nro02;
+//console.log(resultado);
+//var numerico=5;
+//var cadena='hola';
+//var logico= true;
+
+//if(numerico>3)
+//{
+  //console.log("es verdarero.");
+//}else {
+  //console.log("es falso.");
+//}
+
+//if(numerico>3 && cadena=="hola")
+//{
+  //console.log("es verdarero.");
+//}else {
+  //console.log("es falso.");
+//}
+
+//if(numerico>3 || logico==true) //||disyunción
+//{
+  //console.log("es verdarero.");
+//}else {
+  //console.log("es falso.");
+//}
+//estructura de control
+//var numerico=5;
+//var cadena='hola';
+//var logico= false;
+//if(logico) //||disyunción
+//{
+//  console.log("es verdarero.");
+//}else {
+//  console.log("es falso.");
+//}
+//estructuras de repetición
+//for(var i=0; i<6; i++)
+//{
+  //console.log(i);
+//}
+//var i = 0;
+//while(i<5){
+  //console.log(i);
+//i++
+//}
+//vari=10;
+//do{
+  //console.log(i);
+  //i++;
+//}while (i) {
+
+//}
